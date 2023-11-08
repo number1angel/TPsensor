5fc702042f8f4fb6689ac40d48142391bf6943b0
thelmamedran <131172944+thelmamedran@users.noreply.github.com>
2023-11-08 17:48:23 -0300

    Add files via upload

--- conexion.js
+++ conexion.js
@@ -1,42 +1,133 @@
-const mqtt = require('mqtt'); // Asegúrate de tener el paquete MQTT instalado.
+let client;
 
 const clientId = "emqx_node_" + Math.random().toString(16).substring(2, 8);
-const username = "emqx_test"; // ""
-const password = "emqx_test"; // ""
-
-const mqttClient = mqtt.connect("ws://broker.emqx.io:8083/mqtt", {
-  clientId,
-  username,
-  password,
+const username = "";
+const password = "";
+const mqttClient = mqtt.connect("10.0.3.201:1883", {
+    clientId,
+    username,
+    password,
 });
+
+const canvas = document.getElementById("mqttChart");
+const ctx = canvas.getContext("2d");
+const maxDataPoints = 5;
+const chartData = {
+    labels: [],
+    datasets: [
+        {
+            label: 'Datos MQTT R',
+            data: [],
+            borderColor: 'red',
+            fill: false
+        },
+        {
+            label: 'Datos MQTT G',
+            data: [],
+            borderColor: 'green',
+            fill: false
+        },
+        {
+            label: 'Datos MQTT B',
+            data: [],
+            borderColor: 'blue',
+            fill: false
+        }
+    ]
+};
+
+const chart = new Chart(ctx, {
+    type: 'line',
+    data: chartData,
+    options: {
+        scales: {
+            x: {
+                beginAtZero: true
+            },
+            y: {
+                beginAtZero: true,
+                min: 0,
+                max: 255
+            }
+        }
+    }
+});
+
+function updateChart(topic, message) {
+    chartData.labels.push(new Date().toLocaleTimeString());
+    if (topic === "color/R") {
+        chartData.datasets[0].data.push(parseFloat(message));
+        chartData.datasets[1].data.push(null);
+        chartData.datasets[2].data.push(null);
+    } else if (topic === "color/G") {
+        chartData.datasets[0].data.push(null);
+        chartData.datasets[1].data.push(parseFloat(message));
+        chartData.datasets[2].data.push(null);
+    } else if (topic === "color/B") {
+        chartData.datasets[0].data.push(null);
+        chartData.datasets[1].data.push(null);
+        chartData.datasets[2].data.push(parseFloat(message));
+    }
+
+    if (chartData.labels.length > maxDataPoints) {
+        chartData.labels.shift();
+        chartData.datasets[0].data.shift();
+        chartData.datasets[1].data.shift();
+        chartData.datasets[2].data.shift();
+    }
+
+    chart.update();
+}
 
 mqttClient.on("connect", () => {
-  console.log("Client connected: " + clientId);
-  
-  // Suscribirse a los temas deseados.
-  mqttClient.subscribe("color/R");
-  mqttClient.subscribe("color/G");
-  mqttClient.subscribe("color/B");
-});
-
-mqttClient.on("error", (err) => {
-  console.log("Error: ", err);
-  mqttClient.end();
-});
-
-mqttClient.on("reconnect", () => {
-  console.log("Reconnecting...");
+    console.log("Conexión establecida con el servidor MQTT");
+    mqttClient.subscribe("color/R");
+    mqttClient.subscribe("color/G");
+    mqttClient.subscribe("color/B");
 });
 
 mqttClient.on("message", (topic, message) => {
-  console.log(`received message: ${message.toString()} from topic: ${topic}`);
+    console.log(`Mensaje recibido en el tema ${topic}: ${message.toString()}`);
+    updateChart(topic, message.toString());
 });
+ 
+/*
 
-// Puedes usar el cliente MQTT para publicar mensajes si es necesario.
-// mqttClient.publish("tu_tema", "tu_mensaje");
+const broker = 'mqtt://10.0.3.201:1883';
+const options = {
+  username: '', 
+  password: '', 
+};
+const topicR = 'color/R';  
+const topicG = 'color/G';  
+const topicB = 'color/B';  
 
-// Mantén la aplicación en funcionamiento.
-process.on('SIGINT', () => {
-  mqttClient.end();
-  process.exit();
-});
+let dataR = [];
+let dataG = [];
+let dataB = [];
+
+function connectMQTT() {
+  client = mqtt.connect(broker, options);
+  client.on('connect', function () {
+    console.log('Conexión establecida con el servidor MQTT');
+    client.subscribe(topicR, function (err) {
+      if (!err) {
+        console.log('Suscripción exitosa al tema R');
+      }
+    });
+    client.subscribe(topicG, function (err) {
+      if (!err) {
+        console.log('Suscripción exitosa al tema G');
+      }
+    });
+    client.subscribe(topicB, function (err) {
+      if (!err) {
+        console.log('Suscripción exitosa al tema B');
+      }
+    });
+  });
+}
+
+OTRA FORMA DE CONEXION QUE INTENTAMOS PORQUE NO ANDABA 
+*/
+
--- script_mqtt.js
+++ script_mqtt.js
@@ -1,94 +1,5 @@
-const broker = 'mqtt://10.0.3.201:1883';
-        const options = {
-            username: '', 
-            password: '', 
-        };
-        const topicR = 'R'; 
-        const topicG = 'G';
-        const topicB = 'B';
 
-        let dataR = [];
-        let dataG = [];
-        let dataB = [];
-
-        function connectMQTT() {
-            client = mqtt.connect(broker, options);
-            client.on('connect', function () {
-                console.log('Conexión establecida con el servidor MQTT');
-                client.subscribe(topicR, function (err) {
-                    if (!err) {
-                        console.log('Suscripción exitosa al tema R');
-                    }
-                });
-                client.subscribe(topicG, function (err) {
-                    if (!err) {
-                        console.log('Suscripción exitosa al tema G');
-                    }
-                });
-                client.subscribe(topicB, function (err) {
-                    if (!err) {
-                        console.log('Suscripción exitosa al tema B');
-                    }
-                });
-            });
-        }
-
-        function receiveData() {
-            client.on('message', function (topic, message) {
-                console.log('Mensaje recibido en el tema ' + topic + ': ' + message.toString());
-                updateData(topic, message.toString());
-            });
-        }
-
-        function updateChart() {
-            const ctx = document.getElementById('myChart').getContext('2d');
-            const myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: ['R', 'G', 'B'],
-                    datasets: [
-                        {
-                            label: 'Datos del Sensor R',
-                            data: dataR,
-                            borderColor: 'red',
-                            fill: false
-                        },
-                        {
-                            label: 'Datos del Sensor G',
-                            data: dataG,
-                            borderColor: 'green',
-                            fill: false
-                        },
-                        {
-                            label: 'Datos del Sensor B',
-                            data: dataB,
-                            borderColor: 'blue',
-                            fill: false
-                        }
-                    ]
-                },
-                options: {
-                    scales: {
-                        y: {
-                            beginAtZero: true
-                        }
-                    }
-                }
-            });
-        }
-
-        function updateData(topic, message) {
-            if (topic === 'R') {
-                dataR.push(message);
-            } else if (topic === 'G') {
-                dataG.push(message);
-            } else if (topic === 'B') {
-                dataB.push(message);
-            }
-            updateChart();
-        }
-
-/*grafico con numeros randoms*/
+/*grafico con numeros randoms para probar la grafica
 
 var canvas = document.getElementById('randomNumberChart');
 var ctx = canvas.getContext('2d');
@@ -167,15 +78,13 @@ function updateChart() {
 
     chart.update();
     currentX++;
-
-    // Create a horizontal colored gradient bar below the chart
+ 
     var colorGradientContainer = document.getElementById('colorGradientContainer');
     var colorWidth = canvas.width / maxDataPoints;
     var color = `rgb(${randomValue1}, ${randomValue2}, ${randomValue3})`;
     var gradientElement = `<div style="width: ${colorWidth}px; background: ${color}; flex-grow: 1;"></div>`;
     colorGradientContainer.innerHTML = gradientElement + colorGradientContainer.innerHTML;
-
-    // Mantener solo los últimos 5 colores
+ 
     if (colorGradientContainer.children.length > maxDataPoints) {
         colorGradientContainer.removeChild(colorGradientContainer.lastChild);
     }
@@ -189,4 +98,5 @@ scrollBar.addEventListener('change', function () {
     chart.options.scales.x.max = parseInt(scrollBar.value, 10) + maxDataPoints;
     chart.update();
 });
-});
+ 
+*/
